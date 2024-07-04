12c4655304d33792a2d1341c262ba2fb82e2274f
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-04 18:04:44 +0200

    Function returns the string representation of float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+Function returns the string representation of float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Parameters
+        n: float
+
+    Return
+        String representation of n
+    """
+
+    return str(n)
