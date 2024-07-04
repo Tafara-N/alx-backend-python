74d00740bdbe78d57f218edc0eb2085f4df8adeb
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-04 17:45:02 +0200

    Function returns the sum of two floats a and b.

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,7 +1,17 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 
 """
+Function returns the sum of two floats a and b
 """
 
 def add(a: float, b: float) -> float:
+    """
+    Parameters
+        a: float
+        b: float
+
+    Return
+        a + b
+    """
+
     return a + b
