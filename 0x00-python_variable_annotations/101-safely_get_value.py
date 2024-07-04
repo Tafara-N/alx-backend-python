efbede33b825065d493fe36fe370610d97194abd
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-04 23:32:52 +0200

    Function that takes a dictionary and a key and returns the value of the key.

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -12,6 +12,18 @@ T = TypeVar("T")
 
 def safely_get_value(dct: Mapping, key: Any,
                      default: Union[T, None] = None) -> Union[Any, T]:
+    """
+    Parameters:
+        dct (Mapping): The dictionary from which to retrieve the value.
+        key (Any): The key to look for in the dictionary.
+        default (Union[T, None], optional): Default value to return if the key
+                            is not found in the dictionary. Defaults to None.
+
+    Return:
+        The value associated with the specified key in the dictionary if the
+                key is found; otherwise, the default value.
+    """
+
     if key in dct:
         return dct[key]
     else:
