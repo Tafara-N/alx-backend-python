92be49f72511775175b4eb72a8d7a4ef7de3ec13
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-07-08 17:00:42 +0200

    A regular function that takes an int and returns an `asyncio.Task`.

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+
+"""
+Function takes an integer `max_delay` and returns a `asyncio.Task`
+"""
+
+
+import asyncio
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+def task_wait_random(max_delay: int = 10) -> asyncio.Task:
+    """
+    Parameters
+        max_delay: int, default 10
+
+    Return
+        an object: asyncio.Task
+    """
+
+    return asyncio.create_task(wait_random(max_delay))
+
+
+if __name__ == "__main__":
+    task_wait_random(5)
